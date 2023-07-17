d43f9488d889676ef7e390c7ab2c20cbfdcbd4e4
Puleeno Nguyen <puleeno@gmail.com>
2023-07-17 23:12:05 +0700

    Install ORM and simple migration tool

---
+++ migrations/20230717161014_AddUserTables.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddUserTables extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
