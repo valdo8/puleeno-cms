c90f350971751cda62b19c2f2275c05e35838c15
Puleeno Nguyen <puleeno@gmail.com>
2023-07-19 06:48:39 +0700

    Create base code

--- app/core/Application/Middleware/MultilanguageMiddleware.php
+++ app/core/Application/Middleware/MultilanguageMiddleware.php
@@ -2,8 +2,18 @@
 
 namespace App\Application\Middleware;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class MultilanguageMiddleware implements Middleware
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        return $handler->handle($request);
+    }
 }
