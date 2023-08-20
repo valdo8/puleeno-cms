434a3915a54f3181a707faf7eb29b44e5fae0a3d
Puleeno Nguyen <puleeno@gmail.com>
2023-08-20 20:31:05 +0700

    Fix unit test

--- tests/Application/Actions/User/ViewUserActionTest.php
+++ tests/Application/Actions/User/ViewUserActionTest.php
@@ -6,10 +6,10 @@ namespace Tests\Application\Actions\User;
 
 use App\Application\Actions\ActionError;
 use App\Application\Actions\ActionPayload;
-use App\Application\Handlers\HttpErrorHandler;
 use App\Domain\User\User;
 use App\Domain\User\UserNotFoundException;
 use App\Domain\User\UserRepository;
+use App\Http\Handlers\HttpErrorHandler;
 use DI\Container;
 use Slim\Middleware\ErrorMiddleware;
 use Tests\TestCase;
