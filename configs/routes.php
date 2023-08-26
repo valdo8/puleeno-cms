0809dfc0b573dc6be328361d78989a219de0d5bb
Puleeno Nguyen <puleeno@gmail.com>
2023-08-26 10:07:48 +0700

    Load app

--- configs/routes.php
+++ configs/routes.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use App\Core\Application;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
-return function (App $app) {
+return function (Application $app) {
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
         return $response;
