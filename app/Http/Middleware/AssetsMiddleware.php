83db0a7f313833d67920666f6b14ef8146430e11
Puleeno Nguyen <puleeno@gmail.com>
2023-09-15 06:41:59 +0700

    Support parse asset tag via middleware

---
+++ app/Http/Middleware/AssetsMiddleware.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Constracts\MiddlewareConstract;
+use App\Core\HookManager;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Psr7\Response;
+
+class AssetsMiddleware implements MiddlewareConstract
+{
+    public function getPriority(): int
+    {
+        return 9999;
+    }
+
+    protected function getHeadAssets()
+    {
+        ob_start();
+        HookManager::executeAction('head');
+        return ob_get_clean();
+    }
+
+    protected function getOpenBodyTags()
+    {
+        ob_start();
+        HookManager::executeAction('start_body');
+        return ob_get_clean();
+    }
+
+    protected function getFooterAssets()
+    {
+        ob_start();
+        HookManager::executeAction('footer');
+        return ob_get_clean();
+    }
+
+    // Replace template tags by action hooks
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        $template = (string) $response->getBody();
+
+        $template = str_replace([
+            '<!--asset:head-->',
+            '<!--asset:start_body-->',
+            '<!--asset:footer-->'
+        ], [
+            $this->getHeadAssets(),
+            $this->getOpenBodyTags(),
+            $this->getFooterAssets()
+        ], $template);
+
+        $response = new Response();
+        $response->getBody()->write($template);
+
+        return $response;
+    }
+}
