777e831130b00b54a5f987468540aba222c350d2
Puleeno <puleeno@gmail.com>
2023-12-30 20:03:34 +0700

    Create common extension

---
+++ extensions/common/libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'puleeno-cms/common',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1bf6770f09a1b43310bcd4428759f5b95f105cb3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'puleeno-cms/common' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1bf6770f09a1b43310bcd4428759f5b95f105cb3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
